436792f9cd8896913c4ed7b3e30d6249fce4e937
acolon114 <acolon114@usf.edu>
2024-09-16 01:58:31 +0000

    Create updateStock function

--- inventory_management.js
+++ inventory_management.js
@@ -8,7 +8,7 @@ const inventory = [
     { Name: 'Headphones', Price: 100, Quantity: 15, lowStockLevel: 5 },
     { Name: 'Smartwatch', Price: 250, Quantity: 3, lowStockLevel: 1 }
      ];
-     
+
  //Task 2: Create a Function to Display Product Details
 
 function displayProductDetails(product) { 
@@ -18,4 +18,20 @@ function displayProductDetails(product) {
     console.log ("Quantity: " + product['Quantity']);
     
     (product['Quantity'] > product['lowStockLevel']) ? console.log("In Stock") : console.log("Low Stock");
+}
+
+// Task 3: Create a Function to Update Product Stock After Sales
+
+function updateStock(product, unitSold) {
+    
+    if (unitSold <= product['Quantity']) {
+        product['Quantity'] -= unitSold;
+    }
+    if (product['Quantity'] > product['lowStockLevel']) {
+        console.log("In Stock");
+    } else if (product['Quantity'] < product['lowStockLevel'] && product['Quantity'] > 0) {
+        console.log("Low Stock");
+    } else if (product['Quantity'] === 0) {
+        console.log("Out of Stock");
+    }
 }
