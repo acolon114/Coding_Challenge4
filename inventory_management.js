7b3bcfca770f89fdb58de3a8a0eb8d6d69cf0b39
acolon114 <acolon114@usf.edu>
2024-09-16 01:57:21 +0000

    Initialize inventory with product objects"

---
+++ inventory_management.js
@@ -0,0 +1,11 @@
+//Task 1: Create an Inventory Array of Product Objects
+
+const inventory = [ 
+
+    { Name: 'Laptop', Price: 1200, Quantity: 10, lowStockLevel: 3 }, 
+    { Name: 'Smartphone', Price: 800, Quantity: 5, lowStockLevel: 2 },
+    { Name: 'Tablet', Price: 400, Quantity: 7, lowStockLevel: 1 },
+    { Name: 'Headphones', Price: 100, Quantity: 15, lowStockLevel: 5 },
+    { Name: 'Smartwatch', Price: 250, Quantity: 3, lowStockLevel: 1 }
+     ];
+ 
